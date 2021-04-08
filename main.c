ccbf8719475133c46c8104b76022905646597d32
dareenhussein <49562717+dareenhussein@users.noreply.github.com>
2021-04-08 17:33:51 +0200

    Update main.c
    We edited and finalized queTask function and added init function

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ typedef struct t Task;
 typedef struct n node;
 typedef struct q Queue;
 
-Queue* RQ;
+Queue* ReadyQueue;
 Queue* DelayedQueue;
 
 typedef struct t
@@ -24,40 +24,37 @@ typedef struct n
 typedef struct q
 {
 	char type; /* D for delay , R for ready*/
-	node** queue_head; /*the queue is an array of tasks, each has a function pointer and a priority*/
+	node* queue_head; /*the queue is an array of tasks, each has a function pointer and a priority*/
 }Queue;
 
 void QueTask(Task);
 void task1(void);
 
 
-void Init()
-{
-
-}
 
 // Function to push according to priority
 void QueTask(Task task)
 {
 	// Create new Node
-	//Node* temp = newNode(d, p);
 
 	node* current_node;
 	current_node = (node*)malloc(sizeof(node));
 	current_node->task = task;
 
-	node* start = *(RQ->queue_head);
-	//Node* start = (*head);
-
-	if ((*RQ->queue_head)->task.priority > task.priority) /*head has greater priority than current*/
+	node* start = ReadyQueue->queue_head;
+	if (ReadyQueue->queue_head == 0) /*first node*/
+	{
+	ReadyQueue->queue_head = current_node;
+	}
+	else if (ReadyQueue->queue_head->task.priority < task.priority) /*head has less priority than current*/
 	{
 		// Insert New Node before head
-		current_node->next_node = (*(RQ->queue_head));
-		(*(RQ->queue_head)) = current_node;
+		current_node->next_node = ReadyQueue->queue_head;
+		ReadyQueue->queue_head = current_node;
 	}
 	else
 	{
-		while (start->next_node != NULL && start->next_node->task.priority < task.priority) /*traverse till find the  right position*/
+		while (start->next_node != NULL && start->next_node->task.priority > task.priority) /*traverse till find the  right position*/
 		{
 			start = start->next_node;
 		}
@@ -69,41 +66,6 @@ void QueTask(Task task)
 }
 
 
-//void QueTask(Task task) /*function that queues a task in ready queue based on priority*/
-//{
-//	if (RQ->type == 'R') /*the ready queue*/
-//	{
-//		node* current_node;
-//		current_node = (node*)malloc(sizeof(node));
-//
-//		current_node->task = task;
-//		if (RQ->queue_head == 0) /*if queue is empty*/
-//		{
-//			RQ->queue_head->task = task;
-//			RQ->queue_head->next_node = NULL;
-//		}
-//		
-//		else
-//		{
-//			node* temp = RQ->queue_head;
-//			node* temp1 = RQ->queue_head+1;
-//			//for (int i = 0; i < size(RQ->queue_head); i++)
-//			while(temp1->next_node != NULL)
-//			{
-//				if (temp1->task.priority < task.priority)  /*if our priority is higher*/
-//				{
-//					temp->next_node = current_node;
-//					current_node->next_node = temp1;
-//					//RQ->queue_head[i].next_node = RQ->queue_head[i];
-//				}
-//
-//
-//
-//			}
-//		}
-//	}
-//}
-//
 
 void task1() {
 	printf("HIiii \n");
@@ -117,68 +79,54 @@ void task3() {
 	printf("qqqqqq \n");
 }
 
+void Init()
+{
+	/*allocate memory,initialize */
+	ReadyQueue = (Queue *)malloc(sizeof(Queue));
+	DelayedQueue = (Queue *)malloc(sizeof(Queue));
+
+	ReadyQueue->type = 'R';
+	DelayedQueue->type = 'D';
+
+	//initialize the queue
+	ReadyQueue->queue_head = 0;
+}
+
 int main()
 
 {
 	Task A;
 	A.priority = 1;
 	A.func_ptr = task1;
-	A.Task_name = "Task 1";
+	A.Task_name = "Task A";
 
 	Task B;
 	B.priority = 3;
 	B.func_ptr = task2;
-	B.Task_name = "Task 2";
+	B.Task_name = "Task B";
 
 	Task C;
 	C.priority = 2;
 	C.func_ptr = task3;
-	C.Task_name = "Task 3";
-
-	//A.func_ptr(); //runs the function
-
-	RQ = (Queue *) malloc(sizeof(Queue));
-	DelayedQueue = (Queue *)malloc(sizeof(Queue));
+	C.Task_name = "Task C";
 
 
-	RQ->type = 'R';
-	DelayedQueue->type = 'D';
 
-	node* current_node;
-	current_node = (node*)malloc(sizeof(node));
-	current_node->task = A;
-	current_node->next_node = NULL;
+	QueTask(A);
 
-	RQ->queue_head = current_node;
-	//(*(RQ->queue_head))->task = A;
+	printf(ReadyQueue->queue_head->task.Task_name);
+	ReadyQueue->queue_head->task.func_ptr();
+
 	QueTask(B);
-	//printf('\n');
-	
+
+	printf(ReadyQueue->queue_head->task.Task_name);
+	ReadyQueue->queue_head->task.func_ptr();
 
 	QueTask(C);
 
-	printf((*(RQ->queue_head))->task.Task_name);
-	(*(RQ->queue_head))->task.func_ptr();
-	//printf('\n');
-	/*
-	node* current_node;
-	current_node = (node*)malloc(sizeof(node));
-	current_node->task = A;
-	current_node->next_node = NULL;
+	printf(ReadyQueue->queue_head->task.Task_name);
+	ReadyQueue->queue_head->task.func_ptr();
 
-	RQ->queue_head= current_node;
-
-
-
-	printf(RQ->queue_head->task.Task_name);
-	RQ->queue_head->task.func_ptr();
-	printf('\n');
-
-
-
-	printf(RQ->queue_head->task.Task_name);
-	RQ->queue_head->task.func_ptr();
-	*/
 	system("pause");
 	return 0;
 
