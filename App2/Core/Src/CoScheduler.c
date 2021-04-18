8758841a27a70b2138c48cca47386ec4485b5709
marwahmh <52168271+marwahmh@users.noreply.github.com>
2021-04-18 05:09:28 +0200

    Final scheduler

--- App2/Core/Src/CoScheduler.c
+++ App2/Core/Src/CoScheduler.c
@@ -7,22 +7,12 @@
 
 #define TRUE 1
 
-//typedef struct t Task;
-//typedef struct n node;
-//typedef struct q Queue;
-
 
 int count = 0;
 
 
-
 Queue* ReadyQueue;
 Queue* DelayedQueue;
-
-//void QueTask(Task);
-//void task1(void);
-//void DecrementDelayed();
-//void setTimeout(int );
 
 
 Task running_task;
@@ -64,20 +54,14 @@ void QueTask(Task task)
 			start->next_node = current_node;
 		}
 	}
-//	else
-//	{
-//		printf(task.Task_name);
-//		printf( " has priority greater than 8");
-//	}
+
 }
 node* temp3;
 void Dispatch()
 {
-	//Task temp_task;
 	if (ReadyQueue->queue_head != 0) {
 		running_task = ReadyQueue->queue_head->task;
 
-		//func_ptr = ReadyQueue->queue_head->task.func_ptr;
 		if (ReadyQueue->queue_head->next_node != NULL) {
 			temp3=ReadyQueue->queue_head;
 			ReadyQueue->queue_head = ReadyQueue->queue_head->next_node;
@@ -91,10 +75,6 @@ void Dispatch()
 		}
 		running_task.func_ptr();
 	}
-//	else
-//	{
-//		printf("IDLE");
-//	}
 }
 
 void QueDelayedTask(Task task, int delay)
@@ -130,50 +110,12 @@ void QueDelayedTask(Task task, int delay)
 		start->next_node = current_node;
 	}
 
-	//DecrementDelayed();
 
 }
 
 int tickPassed =0;
 
 int deduct_amount=0;
-
-void DecrementDelayed()
-{
-	//tickPassed=0;
-
-//	node* start = DelayedQueue->queue_head;
-
-//	 /*if 50 msec passed*/
-//	//setTimeout(50);
-//	//while(count<=3)  /*to simulate the tick period "3 counts = 1 tick" */
-//	//{
-//	if(tickPassed>=50){
-//		//tickPassed=0;
-//		deduct_amount=tickPassed/50;
-//		tickPassed %= 50;;
-//		int c=0;
-//		while (start != NULL) /*decrement delay of all tasks in the delayed queue*/
-//		{
-//			while(c<deduct_amount){
-//			start->delay -= 1; /*decrement the delay*/
-//			c++;
-//			}
-//			if (start->delay <= 0) /*if the task expires*/
-//			{
-//				QueTask(start->task);
-//				DelayedQueue->queue_head = DelayedQueue->queue_head->next_node;
-//			}
-//		//	if (start->next_node != NULL) {
-//				start = start->next_node; /*move to next task in the queue*/
-//		//	}
-//		}
-//	}
-//	//}
-
-}
-
-
 
 void ReRunMe(int delay)
 {
@@ -185,26 +127,9 @@ void ReRunMe(int delay)
 	{
 		QueDelayedTask(running_task, delay);
 	}
-//	else
-//	{
-//		printf("Delay is invalid !");
-//	}
+
 }
 
-//void task1() {
-//	printf("this is task 1 :) \n");
-//	ReRunMe(3);
-//}
-
-//void task2() {
-//	printf("this is task 2 hellooo \n");
-//	ReRunMe(5);
-
-//}
-
-//void task3() {
-//	printf("this is task 3 hiii \n");
-//}
 
 void Init()
 {
